7d708e0b57f3f43955fe0bc62ece44513f8b6fc3
osueboy <osueboy@hotmail.com>
2015-05-01 12:54:21 -0500

    a

---
+++ _posts/2015-05-01-testing-jekyll-by-osueboy.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "my first post!"
+date:   2015-05-01 11:13:00
+categories: osueboy
+permalink: testing
+---
+Wtf
---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+angular.module('ngMain', [])
+    .controller('layoutController', ['$scope', function($scope) {
+	    $scope.title = 'Titulo';
+}]);
